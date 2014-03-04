954df9f1d7ea87c091028297723f69cc236623df
James Short <james.short@alumni.duke.edu>
2014-03-03 19:03:06 -0500

    add bash completion scripts

---
+++ ssh_config/bash_completion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+_ssh() {
+  local cur config_hosts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+
+  config_hosts=$(sshConfig | tail -n+3 | cut -f 1 -d " ")
+
+  if [[ ! ${cur} == -* ]] ; then
+    COMPREPLY=( $(compgen -W "${config_hosts}" -- ${cur}) )
+    return 0
+  fi
+}
+
+complete -F _ssh ssh
