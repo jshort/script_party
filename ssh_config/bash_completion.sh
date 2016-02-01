9f0fae6e7395b830947edf6f2d91dc1a9cf1f2db
James Short <james.short@alumni.duke.edu>
2016-01-31 22:16:24 -0800

    Major refector of sshConfig (plus rename to ssh-config).
    This fixes the script to handle more than the naive formatting
    of an ssh config file.  This includes whitespaces prior to the
    key name, use of equals sign for key value separation, and lack
    of whitespace between host stanzas.

--- ssh_config/bash_completion.sh
+++ ssh_config/bash_completion.sh
@@ -5,7 +5,7 @@ _ssh() {
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
 
-  config_hosts=$(sshConfig | tail -n+3 | cut -f 1 -d " ")
+  config_hosts=$(ssh-config | tail -n+3 | cut -f 1 -d " ")
 
   if [[ ! ${cur} == -* ]] ; then
     COMPREPLY=( $(compgen -W "${config_hosts}" -- ${cur}) )
